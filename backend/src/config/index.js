29227282d54a863bb2c0ce630689bb6fe6a5247c
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-20 15:18:53 -0700

    Create index.js

---
+++ backend/src/config/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import routes from "./routes.js";
+import { errorHandler } from "./errors/errorHandler.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
+app.use(errorHandler);
+
+export default app;
+
