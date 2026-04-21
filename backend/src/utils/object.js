dc769b84d2e15b3d2203913375069525519844a4
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 10:48:57 -0700

    Update object.js

--- backend/src/utils/object.js
+++ backend/src/utils/object.js
@@ -1,36 +1,11 @@
-export function deepMerge(target = {}, source = {}) {
-  const out = { ...target };
-
-  for (const key of Object.keys(source)) {
-    const sv = source[key];
-    const tv = target[key];
-
-    if (isObject(tv) && isObject(sv)) {
-      out[key] = deepMerge(tv, sv);
-    } else {
-      out[key] = sv;
-    }
-  }
-
-  return out;
+export function merge(target = {}, source = {}) {
+  return { ...target, ...source };
 }
 
-export function omit(obj = {}, keys = []) {
-  const out = {};
-  for (const k of Object.keys(obj)) {
-    if (!keys.includes(k)) out[k] = obj[k];
-  }
-  return out;
+export function deepClone(obj = {}) {
+  return JSON.parse(JSON.stringify(obj));
 }
 
-export function pick(obj = {}, keys = []) {
-  const out = {};
-  for (const k of keys) {
-    if (obj[k] !== undefined) out[k] = obj[k];
-  }
-  return out;
-}
-
-function isObject(v) {
-  return v && typeof v === "object" && !Array.isArray(v);
+export function keys(obj = {}) {
+  return Object.keys(obj);
 }
