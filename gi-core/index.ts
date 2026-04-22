21a3ca5fdd5003e3b92c8342e6bf5728a2ee0c91
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-04-21 17:07:42 -0700

    Refactor exports in index.ts for modules and utils

--- gi-core/index.ts
+++ gi-core/index.ts
@@ -1,10 +1,7 @@
 export * from "./core";
-export * from "./modules/config";
-export * from "./modules/logger";
-export * from "./modules/storage";
-export * from "./modules/access";
-export * from "./modules/roles";
-export * from "./modules/permissions";
-export * from "./modules/users";
-export * from "./modules/events";
-
+export * from "./modules";
+export * from "./utils";
+export * from "./errors";
+export * from "./middleware";
+export * from "./http";
+export * from "./server";
